838229abd018e7e39dd24e7ca4c64df72b59ae8e
unknown <s.ahsan_hasan@hotmail.com>
2019-03-26 03:08:17 +0000

    Update.

--- seeds/crimeSeeder.js
+++ seeds/crimeSeeder.js
@@ -11,9 +11,9 @@ class CrimeSeeder {
         let dates = ['2017-01', '2017-02', '2017-03', '2017-04', '2017-05', '2017-06', '2017-07', '2017-08', '2017-09', '2017-10', '2017-11', '2017-12', '2018-01', '2018-02', '2018-03', '2018-04', '2018-05', '2018-06', '2018-07', '2018-08', '2018-09', '2018-10', '2018-11', '2018-12', '2019-01']
         try {
             let properties = await Property.find({}, 'latitude longitude')
-            let crimeArray = []
             // await forEach(properties, async (element) =>
             for (let j = 0; j < properties.length; j++) {
+                let crimeArray = []
                 for (let i = 0; i < dates.length; i++) {
                     let option = {
                         uri: 'https://data.police.uk/api/crimes-street/all-crime?lat=' +
