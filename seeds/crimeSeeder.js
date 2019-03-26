3924adb9d6bfd043916f19af1f31b89d35892cb3
unknown <s.ahsan_hasan@hotmail.com>
2019-03-26 02:51:23 +0000

    Unique check update.

--- seeds/crimeSeeder.js
+++ seeds/crimeSeeder.js
@@ -2,15 +2,13 @@ const { ErrorHandler } = require('./../utils/ErrorHandler')
 const { Property } = require('./../schema/property')
 const { Crime } = require('./../schema/crime')
 const rp = require('request-promise')
-const url = require('./../utils/url')
-const { forEach } = require('p-iteration')
 
 class CrimeSeeder {
     /**
      * Insert data in crime collection from the api
      */
     static async seed () {
-        let dates = ['2018-11', '2018-12', '2019-01']
+        let dates = ['2017-01', '2017-02', '2017-03', '2017-04', '2017-05', '2017-06', '2017-07', '2017-08', '2017-09', '2017-10', '2017-11', '2017-12', '2018-01', '2018-02', '2018-03', '2018-04', '2018-05', '2018-06', '2018-07', '2018-08', '2018-09', '2018-10', '2018-11', '2018-12', '2019-01']
         try {
             let properties = await Property.find({}, 'latitude longitude')
             let crimeArray = []
@@ -25,15 +23,18 @@ class CrimeSeeder {
                     // console.log(option.uri)
                     await rp(option)
                         .then(function (repos) {
-                            repos.forEach(element => {
-                                let crime = {
-                                    category: element.category,
-                                    latitude: element.location.latitude,
-                                    longitude: element.location.longitude,
-                                    month: element.month,
-                                    crimeId: element.id
+                            repos.forEach(async (element) => {
+                                let checkEntry = await Crime.findOne({ crimeId: element.id })
+                                if (!checkEntry) {
+                                    let crime = {
+                                        category: element.category,
+                                        latitude: element.location.latitude,
+                                        longitude: element.location.longitude,
+                                        month: element.month,
+                                        crimeId: element.id
+                                    }
+                                    crimeArray.push(crime)
                                 }
-                                crimeArray.push(crime)
                             })
                         })
                         .catch(function (err) {
